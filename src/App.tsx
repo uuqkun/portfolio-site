0eb882baaae8b1d7fc02c4823156978f4a40c3ad
Achmad Furqon Rachmadie <uuqkun@gmail.com>
2024-04-06 14:57:59 +0700

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <button className="btn btn-primary">Button</button>
+      </div>
+    </>
+  );
+}
+
+export default App;
