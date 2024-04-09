b6fec32517542301152e06907f785ead7aa54d0e
Achmad Furqon Rachmadie <uuqkun@gmail.com>
2024-04-09 10:35:24 +0700

    Fixed tools icon sizing

--- src/Layouts/ToolsExperiences/ToolsExperiences.tsx
+++ src/Layouts/ToolsExperiences/ToolsExperiences.tsx
@@ -7,15 +7,15 @@ const ToolsExperiences = () => {
         <h2 className="font-bold text-[32px] text-center mb-10">
           Tools & Experiences
         </h2>
-        <div className="flex flex-col lg:flex-row gap-8 items-start w-[80%] m-auto mb-10">
+        <div className="flex flex-col lg:flex-row lg:justify-center gap-8 items-start mb-10">
           {/* tools */}
-          <div className="w-[300px] md:w-[420px] flex flex-wrap gap-4 justify-center lg:justify-start m-auto">
+          <div className="min-w-[300px] md:w-[420px] flex flex-wrap gap-4 justify-center lg:justify-start m-auto lg:m-0">
             {tools
               .filter((tool) => !tool.topSkill)
               .map((tool, index) => (
                 <div
                   key={index}
-                  className="flex flex-col items-center justify-center gap-2 w-[86px] md:w-24 md:h-24 lg:w-28 lg:h-28 rounded-lg p-2"
+                  className="flex flex-col items-center justify-center gap-2 w-[86px] md:w-24 md:h-24 lg:w-32 lg:h-32 rounded-lg p-2 shadow-icon"
                 >
                   <div>
                     <img
@@ -24,15 +24,15 @@ const ToolsExperiences = () => {
                       className="w-8 h-8 lg:w-10 lg:h-10"
                     />
                   </div>
-                  <p className="text-sm lg:text-base mt-4">
+                  <p className="text-xs lg:text-base mt-4">
                     {tool.name}
                   </p>
                 </div>
               ))}
           </div>
           {/* experiences */}
-          <div>
-            <div className="w-full md:w-full m-auto">
+          <div className="m-auto lg:m-0">
+            <div className="w-full m-auto">
               {experiences.map((exp, index) => (
                 <div className="flex justify-between mb-5" key={index}>
                   <div className="w-[200px]">
